84d89c049e3b12a001bc68ea2344686fde1275c8
Trinity Gao <ngao@ucsd.edu>
2022-04-29 23:33:30 -0700

    Update skill_demoTester.java

--- skill_demoTester.java
+++ skill_demoTester.java
@@ -6,7 +6,7 @@ import org.junit.*;
 public class skill_demoTester {
     @Test
     public void Ilove() {
-        assertEquals("I love you so much !", skill_demo.ILove("you")); // test that fail
+        assertEquals("I love you so much!", skill_demo.ILove("you")); // test that pass
     }
 
 }
